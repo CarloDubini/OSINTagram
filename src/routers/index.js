e7256ab9a1bd075b31e32533dd3eed7412aa5261
CarloDubini <carlo.dubini.mar@gmail.com>
2023-03-11 17:11:32 +0100

    preparacion

--- src/routers/index.js
+++ src/routers/index.js
@@ -14,7 +14,18 @@ router.get('/',async (req,res) =>{
     res.render('main',{taskList: lista})
 })
 
-
+router.get('/publicacion:id', async (req,res) =>{
+    
+    //en vez de lista yo quiero datos[id]
+    const querySnapshot= await db.collection('Publicaciones').get()
+    const lista =querySnapshot.docs.map(doc =>({
+        id: doc.id,
+        ...doc.data()
+    }))
+    console.log(lista)
+    
+    res.render('main',{publicacion: lista})
+})
 
 
   module.exports = router;
