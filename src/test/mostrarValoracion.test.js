de9061d5dc2fa574242d9d20bdf18ac9b62d232b
Antoranz <frantora@ucm.es>
2023-03-24 20:49:47 +0100

    Mostrar Mensaje de reportado

---
+++ src/test/mostrarValoracion.test.js
@@ -0,0 +1,70 @@
+const { Router } = require('express');
+const { db } = require('OSINTagram/src/firebase');
+
+beforeAll(async () => {
+    const querySnapshot = await db.collection('Publicaciones').get();
+    lista = querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      valoracion: doc.data().valoracion
+    }));
+  }, 15000);
+
+test('Comprobar que se recibe bien la valoración en la petición con un Id Correcto', async () => {
+    let pasaTest = true;
+    const valoracionEsperada = 4.3;
+    try {
+      const id = 'IwWoulHSw7v7sxAHdOn5';
+      const peticion = await db.collection('Publicaciones').doc(id).get();
+      const publicacion = { id: id, datos: peticion.data() };
+      pasaTest = publicacion.datos.valoracion === valoracionEsperada;
+    } catch (error) {
+      pasaTest = false;
+    }
+    expect(pasaTest).toBe(true);
+}, 20000);
+
+test('Comprobar que todas las valoraciones están en el rango correcto', async () => {
+    let pasaTest = true;
+    const valoracionMinima = 1;
+    const valoracionMaxima = 5;
+    try {
+      const querySnapshot = await db.collection('Publicaciones').get();
+      const publicaciones = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      publicaciones.forEach(publicacion => {
+        const valoracion = publicacion.datos.valoracion;
+        if (valoracion < valoracionMinima || valoracion > valoracionMaxima) {
+          pasaTest = false;
+        }
+      });
+    } catch (error) {
+      pasaTest = false;
+    }
+    expect(pasaTest).toBe(true);
+  }, 20000);
+  
+// 
+// Comprobar que se recibe bien la valoración en la petición con un Id Correcto
+test('Comprobar se recibe bien la valoración en la petición con un Id Correcto', async () => {
+    let pasaTest = true; let datos = {}; let publicacion = {};
+    datos.valoracion = 4.3;
+    try {
+    id = 'IwWoulHSw7v7sxAHdOn5';
+    const peticion = await db.collection('Publicaciones').doc(id).get()
+    publicacion = {id:id, datos : peticion.data()}
+    } catch (error) {
+    pasaTest = false;
+    }
+    pasaTest = pasaTest && (datos.valoracion == publicacion.datos.valoracion);
+    expect(pasaTest).toBe(true);
+    }, 20000);
+
+test('Comprobar que la petición de información con un Id que no existe devuelve undefined en la valoración', async () => {
+        const id = 'IwWoulHSw7v7sxAHdOn1';
+        const peticion = await db.collection('Publicaciones').doc(id).get();
+        const publicacion = {id:id, datos : peticion.data()};
+        expect(publicacion.datos.valoracion).toBeUndefined();
+    }, 20000);
+    
