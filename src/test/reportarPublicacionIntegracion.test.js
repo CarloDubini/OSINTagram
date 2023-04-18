42a1fbba4172329768ee18a05a4ca1890eda7bc0
Antoranz <124276558+Antoranz@users.noreply.github.com>
2023-04-18 15:11:24 +0200

    test de reportar

---
+++ src/test/reportarPublicacionIntegracion.test.js
@@ -0,0 +1,50 @@
+const { TestWatcher } = require("jest");
+const { Router } = require('express');
+const { db } = require('OSINTagram/src/firebase');
+const {mostrarMensajeDeReporte} = require('OSINTagram/src/Controller/publicacionController.js');
+
+test('Comprobar que se aumenta el numero de reportes correctamente', async () => {
+    let pasaTest = true;
+    try{
+        const id = "12324";
+
+        let peticion = await db.collection("Publicaciones").doc(id).get();
+        let publicacion = { id: id, datos: peticion.data() };
+
+        // Obtener el valor actual del atributo "reportes"
+        const reportesAnteriores = publicacion.datos.reportes;
+
+        // Actualizar el valor del atributo "reportes"
+        await db
+            .collection("Publicaciones")
+            .doc(id)
+            .update({ reportes: reportesAnteriores + 1 });
+
+        peticion = await db.collection("Publicaciones").doc(id).get();
+        publicacion = { id: id, datos: peticion.data() };
+        const nuevoNumeroReportes = publicacion.datos.reportes;
+       
+        if(nuevoNumeroReportes != reportesAnteriores+1){
+            pasaTest = false;
+        }
+    }catch(error){
+        pasaTest = false;
+        console.log(error);
+    }
+    expect(pasaTest).toBe(true);
+}, 20000);
+
+afterAll(async () => {
+    const id = "12324";
+    const peticion = await db.collection("Publicaciones").doc(id).get();
+    const publicacion = { id: id, datos: peticion.data() };
+
+    // Obtener el valor actual del atributo "reportes"
+    const reportesNuevos = publicacion.datos.reportes;
+    // Actualizar el valor del atributo "reportes"
+    await db
+        .collection("Publicaciones")
+        .doc(id)
+        .update({ reportes: reportesNuevos - 1 });
+});
+
