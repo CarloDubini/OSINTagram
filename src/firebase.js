a5f2521611fd867a1c5f19dbb92f02fc11bb849f
sirbronn26 <105813754+sirbronn26@users.noreply.github.com>
2023-04-18 16:54:29 +0200

    crearuserautfaltaloin

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,44 @@
-require('dotenv').config()
-const { database } = require('firebase-admin')
-const{initializeApp, applicationDefault}= require('firebase-admin/app')
-const {getFirestore}= require('firebase-admin/firestore')
+// require("dotenv").config();
+// const { database } = require("firebase-admin");
+// const { initializeApp, applicationDefault } = require("firebase-admin/app");
+// const { getFirestore } = require("firebase-admin/firestore");
+// const { getAuth } = require("firebase-admin/auth");
+// //console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+// initializeApp({
+//   credential: applicationDefault(),
+// });
 
-//console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+// const db = getFirestore();
+// const auth = getAuth();
+// console.log("---------------" + auth + "---------------");
+// module.exports = {
+//   db,
+//   auth,
+// };
+require("dotenv").config();
+const { initializeApp, applicationDefault } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const { getAuth } = require("firebase-admin/auth");
+const { signInWithEmailAndPassword } = require("firebase/auth");
+const { createUserWithEmailAndPassword } = require("firebase/auth");
+
+// import {
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+//   onAuthStateChanged,
+//   signOut,
+// } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+
 initializeApp({
-    credential: applicationDefault()
-})
+  credential: applicationDefault(),
+});
 
-const db= getFirestore();
+const db = getFirestore();
+const auth = getAuth();
 
-module.exports= {
-    db,
+module.exports = {
+  db,
+  auth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
 };
--- src/routers/usuarioRouter.js
+++ src/routers/usuarioRouter.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
 const { db } = require("../firebase");
+const { auth } = require("../firebase");
+const { signInWithEmailAndPassword } = require("../firebase");
+const { createUserWithEmailAndPassword } = require("../firebase");
 const {
   mostrarMensajeDeContraseñasNoIguales,
   mostrarMensajeDeUsuarioYaExiste,
@@ -20,30 +23,85 @@ UserRouter.get("/user/:id", async (req, res) => {
 });
 
 //-----------------REGISTRAR USUARIO-----------------
-UserRouter.post("/registrar", async (req, res) => {
-  const { nombreUsuario, contraseña, contraseñaIgual } = req.body;
-  const nuevoUsuario = {
-    nombreUsuario,
-    contraseña,
-  };
-  let mensaje = await mostrarMensajeDeContraseñasNoIguales(
-    nuevoUsuario.contraseña,
-    contraseñaIgual
-  );
-  if (mensaje == "") {
-    mensaje = await mostrarMensajeDeUsuarioYaExiste(nuevoUsuario.nombreUsuario);
-  }
-  if (mensaje == "") {
-    await db.collection("Usuarios").add(nuevoUsuario);
-    mensaje = "Nuevo usuario creado correctamente";
-  }
-  console.log(mensaje);
+UserRouter.get("/registrar", async (req, res) => {
+  const mensaje = "Nuevo ";
   res.render("registrarUsuario", { mensaje });
 });
+// const { nombreUsuario, contraseña, contraseñaIgual } = req.body;
+// const nuevoUsuario = {
+//   nombreUsuario,
+//   contraseña,
+// };
+// let mensaje = await mostrarMensajeDeContraseñasNoIguales(
+//   nuevoUsuario.contraseña,
+//   contraseñaIgual
+// );
+// if (mensaje == "") {
+//   mensaje = await mostrarMensajeDeUsuarioYaExiste(nuevoUsuario.nombreUsuario);
+// }
+// if (mensaje == "") {
+//   await db.collection("Usuarios").add(nuevoUsuario);
+//   mensaje = "Nuevo usuario creado correctamente";
+// }
+// console.log(mensaje);
+// res.render("registrarUsuario", { mensaje });
+UserRouter.post("/registrar", async (req, res) => {
+  const email = req.body.nombreUsuario;
+  const password = req.body.contraseña;
 
+  const auth = getAuth(); // Obtener la instancia de autenticación de Firebase Admin SDK
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log("Usuario creado correctamente:", userCredential.user.toJSON());
+    res.render("registrarUsuario", {
+      mensaje: "Nuevo usuario creado correctamente",
+    });
+  } catch (error) {
+    console.log("Error al crear el usuario:", error);
+    let mensaje = "";
+    if (error.code === "auth/email-already-exists") {
+      mensaje = "El nombre de usuario elegido ya existe";
+    } else {
+      mensaje =
+        "Ha ocurrido un error al crear el usuario. Por favor, inténtalo de nuevo más tarde.";
+    }
+    res.render("registrarUsuario", { mensaje });
+  }
+});
 // -----------------LOGIN USUARIO-----------------
 UserRouter.get("/login", async (req, res) => {
   res.render("iniciarSesion");
+});
+UserRouter.post("/login", async (req, res) => {
+  const email = req.body.nombreUsuario;
+  const password = req.body.contraseña;
+
+  const firebaseAuth = auth; // Obtener la instancia de autenticación de Firebase Admin SDK
+
+  firebaseAuth
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      console.log("Inicio de sesión correcto");
+      res.render("main", { user: userCredential.user.toJSON() });
+    })
+    .catch((error) => {
+      console.log("Error al iniciar sesión:", error);
+      let mensaje = "";
+      if (error.code === "auth/user-not-found") {
+        mensaje = "El usuario no existe";
+      } else if (error.code === "auth/wrong-password") {
+        mensaje = "Contraseña incorrecta";
+      } else {
+        mensaje =
+          "Ha ocurrido un error al iniciar sesión. Por favor, inténtalo de nuevo más tarde.";
+      }
+      res.render("iniciarSesion", { mensaje });
+    });
 });
 
 module.exports = { UserRouter };
