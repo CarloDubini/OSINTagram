551741844145410d28a672dc3ef4405086e03b04
sirbronn26 <105813754+sirbronn26@users.noreply.github.com>
2023-04-18 10:49:12 +0200

    inisesionvista

--- src/routers/publicacionRouter.js
+++ src/routers/publicacionRouter.js
@@ -1,103 +1,109 @@
-const {Router} = require('express')
-const {db}= require('../firebase')
-const {ordenarAlfabeticamente,mostrarMensaje,buscarPorPalabraClave,obtenerDatosPorTitulo,pruebaDatosPorTítulo,pruebaBusquedaPorPalabraClave,
-    mostrarMensajeDeReporte}= require('../Controller/publicacionController.js')
+const { Router } = require("express");
+const { db } = require("../firebase");
+const {
+  ordenarAlfabeticamente,
+  mostrarMensaje,
+  buscarPorPalabraClave,
+  obtenerDatosPorTitulo,
+  pruebaDatosPorTítulo,
+  pruebaBusquedaPorPalabraClave,
+  mostrarMensajeDeReporte,
+} = require("../Controller/publicacionController.js");
 
 const PublicacionRouter = Router();
 
-
 //-------------------GETTS y POSTS---------------
-PublicacionRouter.get('/',async (req,res) =>{
-    const querySnapshot= await db.collection('Publicaciones').get()
-    const lista =querySnapshot.docs.map(doc =>({
-        id: doc.id,
-        ...doc.data()
-    }))
-    ordenarAlfabeticamente(lista);
-    // coger la foto para la valoracion
-    const fotovalQuerySnapshot = await db.collection('fotoval').get();
-    const fotoval = fotovalQuerySnapshot.docs.map((doc) => doc.data().link);
+PublicacionRouter.get("/", async (req, res) => {
+  const querySnapshot = await db.collection("Publicaciones").get();
+  const lista = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  ordenarAlfabeticamente(lista);
+  // coger la foto para la valoracion
+  const fotovalQuerySnapshot = await db.collection("fotoval").get();
+  const fotoval = fotovalQuerySnapshot.docs.map((doc) => doc.data().link);
 
-    res.render('main',{taskList: lista, fotoval:fotoval})
-})
+  res.render("main", { taskList: lista, fotoval: fotoval });
+});
 
+PublicacionRouter.get("/publicacion:id", async (req, res) => {
+  //en vez de lista yo quiero datos[id]
+  const querySnapshot = await db.collection("Publicaciones").get();
+  const lista = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  ordenarAlfabeticamente(lista);
 
-PublicacionRouter.get('/publicacion:id', async (req,res) =>{
-    //en vez de lista yo quiero datos[id]
-    const querySnapshot= await db.collection('Publicaciones').get()
-    const lista =querySnapshot.docs.map(doc =>({
-        id: doc.id,
-        ...doc.data()
-    }))
-    ordenarAlfabeticamente(lista);
-
-    res.render('main',{publicacion: lista})
-})
+  res.render("main", { publicacion: lista });
+});
 //----------------VER CADA PUBLICACION-------------
-PublicacionRouter.get('/publicacion/:id', async (req,res) =>{
-
-    let id = req.params.id
-    const peticion = await db.collection('Publicaciones').doc(id).get()
-    const publicacion = {id:id, datos : peticion.data()}
-    console.log("--------------------HE CLICKADO EN LA PUBLICACION:---------------------")
-    console.log(publicacion)
-    let mensaje = mostrarMensajeDeReporte(publicacion.datos.reportes)
-    console.log('reportes:',publicacion.datos.reportes,'msg:',mensaje)
-    res.render('publicacion',{publicacion, mensaje}) 
-})
+PublicacionRouter.get("/publicacion/:id", async (req, res) => {
+  let id = req.params.id;
+  const peticion = await db.collection("Publicaciones").doc(id).get();
+  const publicacion = { id: id, datos: peticion.data() };
+  console.log(
+    "--------------------HE CLICKADO EN LA PUBLICACION:---------------------"
+  );
+  console.log(publicacion);
+  let mensaje = mostrarMensajeDeReporte(publicacion.datos.reportes);
+  console.log("reportes:", publicacion.datos.reportes, "msg:", mensaje);
+  res.render("publicacion", { publicacion, mensaje });
+});
 
 //----------------REPORTAR-------------
-PublicacionRouter.get('/publicacion/:id/reportar', async (req,res) =>{
+PublicacionRouter.get("/publicacion/:id/reportar", async (req, res) => {
+  let id = req.params.id;
+  const peticion = await db.collection("Publicaciones").doc(id).get();
+  const publicacion = { id: id, datos: peticion.data() };
 
-    let id = req.params.id
-    const peticion = await db.collection('Publicaciones').doc(id).get()
-    const publicacion = {id:id, datos : peticion.data()}
-    
-    // Obtener el valor actual del atributo "reportes"
-    const reportesAnteriores = publicacion.datos.reportes;
-    
-    // Actualizar el valor del atributo "reportes"
-    await db.collection('Publicaciones').doc(id).update({ reportes: reportesAnteriores + 1 });
+  // Obtener el valor actual del atributo "reportes"
+  const reportesAnteriores = publicacion.datos.reportes;
 
-    res.redirect(`/publicacion/${id}`);
-})
+  // Actualizar el valor del atributo "reportes"
+  await db
+    .collection("Publicaciones")
+    .doc(id)
+    .update({ reportes: reportesAnteriores + 1 });
+
+  res.redirect(`/publicacion/${id}`);
+});
 //----------------BUSCAR POR PALABRA CLAVE-------------
-PublicacionRouter.get('/search', async (req,res) =>{
+PublicacionRouter.get("/search", async (req, res) => {
+  const palabraBuscada = req.query.searchInput;
+  const querySnapshot = await db.collection("Publicaciones").get();
+  const lista = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
 
-    const palabraBuscada = req.query.searchInput;
-    const querySnapshot= await db.collection('Publicaciones').get()
-    const lista =querySnapshot.docs.map(doc =>({
-        id: doc.id,
-        ...doc.data()
-    }))
+  const fotovalQuerySnapshot = await db.collection("fotoval").get();
+  const fotoval = fotovalQuerySnapshot.docs.map((doc) => doc.data().link);
+  const resultado = await buscarPorPalabraClave(palabraBuscada, lista);
 
-    const fotovalQuerySnapshot = await db.collection('fotoval').get();
-    const fotoval = fotovalQuerySnapshot.docs.map((doc) => doc.data().link);
-    const resultado = await buscarPorPalabraClave(palabraBuscada, lista)
-
-    res.render('main',{taskList: resultado, fotoval:fotoval});
-})
+  res.render("main", { taskList: resultado, fotoval: fotoval });
+});
 
 //-----------------CREAR PUBLICACION-----------------
-PublicacionRouter.get('/crear', (req,res) =>{
-    res.render('crearPublicacion')
-})
+PublicacionRouter.get("/crear", (req, res) => {
+  res.render("crearPublicacion");
+});
 
-PublicacionRouter.post('/crear', async (req,res) =>{
-    const {titulo, localizacion, descripcion, imagen} = req.body;
-    const nuevaPublicacion = {
-        titulo,
-        localizacion,
-        descripcion,
-        imagen,
-        reportes: 0
-    }
-    await db.collection('Publicaciones').add(nuevaPublicacion)
-    res.redirect('/publicacion')
-})
+PublicacionRouter.post("/crear", async (req, res) => {
+  const { titulo, localizacion, descripcion, imagen } = req.body;
+  const nuevaPublicacion = {
+    titulo,
+    localizacion,
+    descripcion,
+    imagen,
+    reportes: 0,
+  };
+  await db.collection("Publicaciones").add(nuevaPublicacion);
+  res.redirect("/publicacion");
+});
 
 //-----------FUNCIONES VARIAS--------------
 //están en publicacionController.js así que no hace falta ponerlas aquí
 
-
-module.exports = {PublicacionRouter};
+module.exports = { PublicacionRouter };
--- src/routers/usuarioRouter.js
+++ src/routers/usuarioRouter.js
@@ -1,43 +1,55 @@
-const {Router} = require('express')
-const {db}= require('../firebase')
-const {mostrarMensajeDeContraseñasNoIguales,
-    mostrarMensajeDeUsuarioYaExiste, mostrarMensaje}= require('../Controller/userController.js')
+const { Router } = require("express");
+const { db } = require("../firebase");
+const {
+  mostrarMensajeDeContraseñasNoIguales,
+  mostrarMensajeDeUsuarioYaExiste,
+  mostrarMensaje,
+} = require("../Controller/userController.js");
 
 const UserRouter = Router();
 
-UserRouter.get('/user/:id', async (req,res) =>{
-    let id = req.params.id
-    const peticion = await db.collection('Usuarios').doc(id).get()
-    const usuario = {id:id, datos : peticion.data()}
-    console.log("--------------------HE CLICKADO EN EL USUARIO:---------------------")
-    console.log(usuario)
-    res.render('user',{usuario}) 
-})
+UserRouter.get("/user/:id", async (req, res) => {
+  let id = req.params.id;
+  const peticion = await db.collection("Usuarios").doc(id).get();
+  const usuario = { id: id, datos: peticion.data() };
+  console.log(
+    "--------------------HE CLICKADO EN EL USUARIO:---------------------"
+  );
+  console.log(usuario);
+  res.render("user", { usuario });
+});
 
 //-----------------REGISTRAR USUARIO-----------------
-UserRouter.get('/registrar', (req,res) =>{
-    let mensaje = "";
-    res.render('registrarUsuario', {mensaje})
-})
+UserRouter.get("/registrar", (req, res) => {
+  let mensaje = "";
+  res.render("registrarUsuario", { mensaje });
+});
 
-UserRouter.post('/registrar', async (req,res) =>{
-    const {nombre, nombreUsuario, contraseña, contraseñaIgual} = req.body;
-    const nuevoUsuario = {
-        nombre,
-        nombreUsuario,
-        contraseña,
-    }
+UserRouter.post("/registrar", async (req, res) => {
+  const { nombre, nombreUsuario, contraseña, contraseñaIgual } = req.body;
+  const nuevoUsuario = {
+    nombre,
+    nombreUsuario,
+    contraseña,
+  };
 
-    let mensaje = mostrarMensajeDeContraseñasNoIguales(nuevoUsuario.contraseña, contraseñaIgual)
-    if(mensaje == ""){
-        mensaje = mostrarMensajeDeUsuarioYaExiste(nuevoUsuario.nombreUsuario)
-    }
+  let mensaje = mostrarMensajeDeContraseñasNoIguales(
+    nuevoUsuario.contraseña,
+    contraseñaIgual
+  );
+  if (mensaje == "") {
+    mensaje = mostrarMensajeDeUsuarioYaExiste(nuevoUsuario.nombreUsuario);
+  }
 
-    if(mensaje == ""){
-        await db.collection('Usuarios').add(nuevoUsuario)
-        mensaje = "Nuevo usuario creado correctamente"
-    }
-    res.render('registrarUsuario',{mensaje}) 
-})
+  if (mensaje == "") {
+    await db.collection("Usuarios").add(nuevoUsuario);
+    mensaje = "Nuevo usuario creado correctamente";
+  }
+  res.render("registrarUsuario", { mensaje });
+});
+//-----------------LOGIN USUARIO-----------------
+UserRouter.get("/login", async (req, res) => {
+  res.redirect("iniciarSesion");
+});
 
-module.exports = {UserRouter}
+module.exports = { UserRouter };
