887de3adacf255f958773de2ed8d038a52c29ae3
Victor Ortego <ortegovictor4@gmail.com>
2023-04-23 19:46:04 +0200

    TestInciarSesion acabados

--- src/test/inicioSesionBasicoU.test.js
+++ src/test/inicioSesionBasicoU.test.js
@@ -1,7 +1,7 @@
 const { TestWatcher } = require("jest");
 const { Router } = require('express');
 const { db } = require('OSINTagram/src/firebase');
-const {} = require('OSINTagram/src/Controller/userController.js');
+const {obtenerUsuarioPorNombredeUsuario} =  require('OSINTagram/src/Controller/tests.js');
 
 let lista = [];
 beforeAll(async () => {
@@ -9,57 +9,54 @@ beforeAll(async () => {
         {
             nombreUsuario: "osintagram",
             contraseña: "123456",
+            logueado: "0",
         },
         {
             nombreUsuario: "pepe22",
             contraseña: "pepemanuel",
+            logueado: "0",
         },
         {
             nombreUsuario: "manuel45",
             contraseña: "manuelpepe",
+            logueado: "0",
         }
       ];
 }, 15000);
 
 test('Comprobar que la funcion de iniciar sesión funciona correctamente cuando se introducen un usuario y una contreseña correctas', async () => {
-     let pasaTest = true;
-//     try{
-//         const id = 'IwWoulHSw7v7sxAHdOn5';
-//         const peticion = await db.collection('Publicaciones').doc(id).get()
-//         const numReportes = {id:id, datos : peticion.data().reportes}
-//         mostrarMensajeDeReporte(numReportes.datos);
-//     }catch(error){
-//         pasaTest = false;
-//     }
+    let pasaTest = true;
+    try{
+        const nombreUsuario = 'manuel45';
+        const contraseña = 'manuelpepe';
+        usuario = obtenerUsuarioPorNombredeUsuario(nombreUsuario, lista);
+        if(contraseña !== usuario.contraseña) pasaTest = false;
+    }catch(error){
+        pasaTest = false;
+    }
      expect(pasaTest).toBe(true);
 }, 20000);
 
  test('Comprobar que la funcion de iniciar sesión funciona correctamente cuando se introducen un usuario que no existe y una contreseña', async () => {
-     let pasaTest = true;
-//     let mensajeEsperado = 'Esta publicacion ha sido reportada por varios usuarios y puede ser falsa';
-//     let mensajeGenerado;
-//     try{
-//         const id = 'pKt4lzyXhGbxyYqWAnkC';
-//         const peticion = await db.collection('Publicaciones').doc(id).get()
-//         const numReportes = {id:id, datos : peticion.data().reportes}
-//         mensajeGenerado = mostrarMensajeDeReporte(numReportes.datos);
-//     }catch(error){
-//         pasaTest = false;
-//     }
-     expect(mensajeGenerado).toBe(mensajeEsperado);
+    let pasaTest = true;
+    try{
+        const nombreUsuario = 'manuel44444';
+        usuario = obtenerUsuarioPorNombredeUsuario(nombreUsuario,lista);
+    }catch(error){
+        pasaTest = false;
+    }
+     expect(usuario).toBe("");
  }, 20000);
 
  test('Comprobar que la funcion de iniciar sesión funciona correctamente cuando se introducen un usuario que existe y una contreseña incorrecta', async () => {
-     let pasaTest = true;
-//     let mensajeEsperado = '';
-//     let mensajeGenerado;
-//     try{
-//         const id = 'IwWoulHSw7v7sxAHdOn5';
-//         const peticion = await db.collection('Publicaciones').doc(id).get()
-//         const numReportes = {id:id, datos : peticion.data().reportes}
-//         mensajeGenerado = mostrarMensajeDeReporte(numReportes.datos);
-//     }catch(error){
-//         pasaTest = false;
-//     }
-     expect(mensajeGenerado).toBe(mensajeEsperado);
+    let pasaTest = true;
+    try{
+        const nombreUsuario = 'manuel45';
+        const contraseña = 'manuelpepeeeee';
+        usuario = obtenerUsuarioPorNombredeUsuario(nombreUsuario,lista);
+        if(contraseña !== usuario.contraseña) pasaTest = false;
+    }catch(error){
+        pasaTest = false;
+    }
+     expect(pasaTest).toBe(false);
  }, 20000);
