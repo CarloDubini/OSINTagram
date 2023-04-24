21e0f5a2a6a8c0deb9262f90d64dedff222ce69d
CarloDubini <carlo.dubini.mar@gmail.com>
2023-04-24 20:56:53 +0200

    funciona :_)

--- src/routers/publicacionRouter.js
+++ src/routers/publicacionRouter.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
     cb(null, "src/uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, file.originalname);
   },
 });
 const upload = multer({ storage: storage });
